adbec668116252a35ff1223cd16866c17cd76d07
sarayv9026 <sarayqvsao@gmail.com>
2024-11-23 21:52:24 -0500

    fix: Se corrige array de distancias

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
     //Elementos necesarios para todas las acciones.
     static Scanner scanner = new Scanner(System.in);
     static String[] planetas = {"Marte", "Júpiter", "Saturno", "Urano", "Neptuno", "Venus", "Mercurio"};
-    static double[] distancias = {54.6, 628.7, 1201,2723,4347,40,77};
+    static double[] distancias = {132.83, 617.54, 1.3676, 2.7774, 4.3982, 156.94, 140.17};
     static String[] descripcionPlaneta = {"Conocido como el planeta rojo debido al óxido de hierro en su superficie","El planeta más grande del sistema solar, compuesto principalmente por hidrógeno y helio","El elemento más llamativo de Saturno son sus 7 anillos, los cuales se cree que están compuestos millones de pedazos de hielo y roca procedentes cometas, asteroides o lunas","Se trata del primero de los gigantes helados de nuestro sistema planetario y está compuesto por una especie de hielo fluido de agua, amoníaco y metano","Oscuro, frío y regido por vientos supersónicos, el gigante de hielo Neptuno es el octavo y más distante planeta de nuestro sistema solar","Las densas nubes de ácido sulfúrico que rodean la atmosfera de Venus generan tal efecto invernadero que hace de Venus el planeta más caliente del sistema solar","Con un tamaño tan solo un poco mayor al de nuestra luna, es el planeta más pequeño del sistema solar y el más cercano al Sol"};
     static String[] naves = {"Exploradora", "carga pesada", "Velocidad máxima"};
     static double[] velocidades = {20000.0, 15000.0, 30000};
